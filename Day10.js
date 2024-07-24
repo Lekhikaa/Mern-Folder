07da79d3ac166ca984c35fbaaece8f2c65339869
unknown <np01cp4s230088@islingtoncollege.edu.np>
2024-07-24 09:49:02 +0545

    Adding

---
+++ Day10.js
@@ -0,0 +1,28 @@
+// if else
+let willRain = true
+if(willRain){
+    console.log("take umbrella");
+}else{
+    console.log("no need to take umbrella");
+}
+
+let students=[
+    {
+        name:"ram",
+        paidStatus:false
+    },
+    {
+        name:"hari",
+        paidStatus:true
+    },
+    {
+        name:"sita",
+        paidStatus:false
+    },
+
+]
+
+// Output:
+// Ram cannot give exam
+// Hari can give exam 
+// Sita cannot give exam
